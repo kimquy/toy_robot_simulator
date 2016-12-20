b5c92a187c7e72cd27675558579f119949a34265
Long Nguyen <long.polyglot@gmail.com>
2016-12-20 16:11:24 +0700

    Code refactor

--- src/robot.rb
+++ src/robot.rb
@@ -5,7 +5,7 @@ class Robot
 
   attr_reader :board, :instruction, :state
 
-  def initialize(state = State.new, board, instruction)
+  def initialize(board, instruction, state = State.new)
     @state = state
     @board = board
     @instruction = instruction
@@ -15,14 +15,14 @@ class Robot
     instruction.moves.each do |move|
       case move.split(" ").first
         when "MOVE" then move_robot
-        when "LEFT" then turn_left
-        when "RIGHT" then turn_right
+        when "LEFT" then turn_to(:left)
+        when "RIGHT" then turn_to(:right)
         when "PLACE" then place_robot(move.split(" ")[1])
         when "REPORT" then report
         else puts "Invalid move #{move}"
       end
     end
-    
+
     report
   end
 
@@ -39,37 +39,29 @@ class Robot
     state.update(x: x, y: y, facing: facing) unless board.off_board?(x, y)
   end
 
-  def turn_left
-    case state.facing
-      when "NORTH" then state.facing = "WEST"
-      when "SOUTH" then state.facing = "EAST"
-      when "WEST" then state.facing = "SOUTH"
-      when "EAST" then state.facing = "NORTH"
-      else "Invalid"
-    end
-  end
-
-  def turn_right
-    case state.facing
-      when "NORTH" then state.facing = "EAST"
-      when "SOUTH" then state.facing = "WEST"
-      when "WEST" then state.facing = "NORTH"
-      when "EAST" then state.facing = "SOUTH"
-      else "Invalid"
-    end
+  def turn_to(direction)
+    state.facing =
+      case state.facing
+        when "NORTH" then direction == :left ? "WEST" : "EAST"
+        when "SOUTH" then direction == :left ? "EAST" : "WEST"
+        when "WEST" then direction == :left ? "SOUTH" : "NORTH"
+        when "EAST" then direction == :left ? "NORTH" : "SOUTH"
+      end
   end
 
   def move_robot
     case state.facing
-      when "NORTH"
-        state.y += 1 unless board.off_board?(state.x, state.y + 1)
-      when "SOUTH"
-        state.y -= 1 unless board.off_board?(state.x, state.y - 1)
-      when "WEST"
-        state.x -= 1 unless board.off_board?(state.x - 1, state.y)
-      when "EAST"
-        state.x += 1 unless board.off_board?(state.x + 1, state.y)
-      else
+    when "NORTH" then update_position(state.x, state.y + 1)
+    when "SOUTH" then update_position(state.x, state.y - 1)
+    when "WEST" then update_position(state.x - 1, state.y)
+    when "EAST" then update_position(state.x + 1, state.y)
+    end
+  end
+
+  def update_position(x, y)
+    unless board.off_board?(x, y)
+      state.x = x
+      state.y = y
     end
   end
 
