2a72008d5b19c48cbb3f3322ffb1adc1c2a063f9
Long Nguyen <long.polyglot@gmail.com>
2017-01-05 23:13:22 +0700

    Wording refactor

--- src/robot.rb
+++ src/robot.rb
@@ -18,31 +18,37 @@ class Robot
   end
 
   def perform
-    while moves.size.positive? && state.facing.nil?
-      place_robot(moves.first)
-      @moves = @moves.drop(1) if state.facing.nil?
-    end
-
-    moves.each do |move|
-      action, location = move.split(" ")
-      case action
-        when "MOVE" then move_robot
-        when "LEFT" || "RIGHT" then turn_to(action)
-        when "PLACE" then place_robot(location)
-        when "REPORT" then report
-      end
-    end
-
+    place_robot_on_board
+    run_all_the_rest_moves
     report
   end
 
   private
 
+  def place_robot_on_board
+    while moves.size.positive? && state.facing.nil?
+      place_robot_to(moves.first)
+      @moves = @moves.drop(1) if state.facing.nil?
+    end
+  end
+
+  def run_all_the_rest_moves
+    moves.each do |move|
+      action, location = move.split(" ")
+      case action
+        when "MOVE" then move_robot_forward
+        when "LEFT" || "RIGHT" then turn_to(action)
+        when "PLACE" then place_robot_to(location)
+        when "REPORT" then report
+      end
+    end
+  end
+
   def report
     state.to_s
   end
 
-  def place_robot(location)
+  def place_robot_to(location)
     @state = PlacingReducer.exec(state, location, board)
   end
 
@@ -50,7 +56,7 @@ class Robot
     @state = DirectionReducer.exec(state, direction)
   end
 
-  def move_robot
+  def move_robot_forward
     @state = MovementReducer.exec(state, board)
   end
 end
